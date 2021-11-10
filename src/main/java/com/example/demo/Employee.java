e9f70cd971020e3cae87a0ccbd916fc7b77121b2
KavingaWeerasinghe <weerasinghekavinga@gmail.com>
2021-11-10 17:35:23 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.annotation.Generated;
+
+
+@Data
+@Document
+public class Employee {
+
+    @Id
+    private Integer id;
+    private String fName;
+    private String lName;
+    private String address;
+
+    public Employee(String fName, String lName, String address) {
+        this.fName = fName;
+        this.lName = lName;
+        this.address = address;
+    }
+
+
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
---
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -0,0 +1,55 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("api/v1")
+public class EmployeeController {
+
+    @Autowired
+    public EmployeeRepository employeeRepository ;
+
+
+    @PostMapping("/employee")
+    public Employee createEmployee(@RequestBody Employee employee){
+       return employeeRepository.save(employee);
+
+    }
+
+    @GetMapping("employee/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable(value="id")String id ) throws ResourceNotFoundException {
+      Employee employee =  employeeRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Not Found"));
+        return  ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("employee/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable(value="id")String id , @RequestBody Employee empDetails) throws ResourceNotFoundException {
+        Employee employee =employeeRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Not Found"));
+
+        employee.setfName(empDetails.getfName());
+        employee.setlName(empDetails.getlName());
+        employee.setAddress(empDetails.getAddress());
+        employeeRepository.save(employee);
+        return ResponseEntity.ok().body(employee);
+    }
+
+    @DeleteMapping("employee/{id}")
+    public String deleteEmployee(@PathVariable(value="id")String id){
+        employeeRepository.deleteById(id);
+        return "Employee Deleted";
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+       return employeeRepository.findAll();
+    }
+
+
+
+}
