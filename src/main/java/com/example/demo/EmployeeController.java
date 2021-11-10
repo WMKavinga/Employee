6b1cee4da52ae851fb32bc4dd1ff9248796c363e
KavingaWeerasinghe <weerasinghekavinga@gmail.com>
2021-11-10 18:25:04 +0530

    Comment to Controller class

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+//Comment to Controller class
 @RestController
 @RequestMapping("api/v1")
 public class EmployeeController {
