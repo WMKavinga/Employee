f369a2f86589b7f969a7d918831c2768e11c8e3e
KavingaWeerasinghe <weerasinghekavinga@gmail.com>
2021-11-10 19:12:46 +0530

    Added Comments repo master

--- src/main/java/com/example/demo/EmployeeRepository.java
+++ src/main/java/com/example/demo/EmployeeRepository.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
-
+//Repo Class
 @Component
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
 }
